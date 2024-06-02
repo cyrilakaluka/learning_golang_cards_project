3852807ef1c27a57b5a7595ed5be8d4dc0534529
Akaluka Chibueze Cyril <akalukacyril@gmail.com>
2024-06-02 14:31:27 +0300

    Go > 1.2 seeds by default

--- deck.go
+++ deck.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"time"
 )
 
 type deck []string
@@ -70,11 +69,8 @@ func newDeckFromFile(filename string) deck {
 }
 
 func (d deck) shuffle() {
-	source := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(source)
-
 	for i := range d {
-		newPosition := r.Intn(len(d) - 1)
+		newPosition := rand.Intn(len(d) - 1)
 		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
